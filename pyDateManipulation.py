280f6cdca971228ae25707e80388f626852c64f7
Delvidio Neto <delvidio.neto@outlook.com>
2023-08-14 10:30:10 -0300

    feat: Inclusão da regra Month

--- pyDateManipulation.py
+++ pyDateManipulation.py
@@ -29,16 +29,21 @@ class intnx():
         self._Increment = Increment
         self._Alignment = Alignment
         self._EndDate = ''
-        self.varDate = datetime.strptime(Date, '%Y-%m-%d')
+
+        if type(Date) == str:        
+            self._Date = datetime.strptime(Date, '%Y-%m-%d')
+        else:
+            self._Date =  datetime.combine(Date, datetime.min.time())
+
 
     def getDates(self):
         """ getDates: Método que trás o dia de forma corrente YYYY-MM-DD """
 
-        vDayClass = self.varDate.day
-        vMonthClass = self.varDate.month
-        vYearClass = self.varDate.year
+        vDayClass = self._Date.day
+        vMonthClass = self._Date.month
+        vYearClass = self._Date.year
         
-        def __getAlignment(vDayMeth, vMonthMeth, vYearMeth):
+        def __getAlignment(vYearMeth, vMonthMeth, vDayMeth):
             
             if self._Alignment.upper() == "B":
                 vDateMeth = date(vYearMeth, vMonthMeth, 1)
@@ -50,78 +55,73 @@ class intnx():
                 endOfMonth = calendar.monthrange(vYearMeth, vMonthMeth)
                 vDateMeth = date(vYearMeth, vMonthMeth, endOfMonth[1])
 
-            return vDateMeth
+            return datetime.combine(vDateMeth, datetime.min.time())
         
 
         if self._Interval.upper() == "YEAR":
 
             vInterYear = abs(vYearClass + (self._Increment))      
-            vGetDate = __getAlignment(vDayClass, vMonthClass, vInterYear)   
+            vGetDate = __getAlignment(vInterYear, vMonthClass, vDayClass)   
             vInterDate = date(vGetDate.year, vGetDate.month ,vGetDate.day)
 
         elif self._Interval.upper() == "MONTH":
-            
+            i = 1
+            vMonth = vMonthClass
+            vYear = vYearClass
             monthThirtyOneDays = [1, 3, 5, 7, 8, 10, 12]
             monthThirtyDays = [4, 6, 9, 11] 
+            cDays = 0
 
-            endOfMonth = calendar.monthrange(ano, mes)
+            while i <= abs(self._Increment):
+                if self._Increment < 0:
+                    if vMonth == 1:
+                        vMonth = 13
+                        vYear = vYear - 1
+                    vMonth = vMonth - 1
+                else: 
+                    if vMonth == 12:
+                        vMonth = 0
+                        vYear = vYear + 1
 
-            for iThirtyOneDays in monthThirtyOneDays:
-                if iThirtyOneDays == vMonthClass:
-                    validMonth = True
-                else:
-                    validMonth = False
+                    vMonth = vMonth + 1
 
-            for iThirtyDays in monthThirtyDays:
-                if iThirtyDays == vMonthClass:
-                    validMonth = True
-                else:
-                    validMonth = False
+                for iThirtyOneDays in monthThirtyOneDays:
+                    if iThirtyOneDays == vMonth:
+                        cDays =  cDays + 31
+                    else:
+                        vldMonthThirtyOne = False
 
+                if vldMonthThirtyOne == False:
+                    for iThirtyDays in monthThirtyDays:
+                        if iThirtyDays == vMonth:
+                            cDays =  cDays + 30
 
+                if calendar.isleap(vYear) == True and vMonth== 2:
+                    cDays =  cDays + 29
 
-    #         if arDia[1] == 28 and self._Increment < 0:
-    #             ldom = 31 * self._Increment
+                elif calendar.isleap(vMonth) == False and vMonth == 2:
+                    cDays =  cDays + 28
+                i = i + 1
 
-    #         elif arDia[1] == 29 and self._Increment < 0:
-    #             ldom = 30 * self._Increment
+            if self._Increment < 0:
+                interDate = self._Date + timedelta(days=(cDays * -1))
+            elif self._Increment > 0:
+                interDate = self._Date + timedelta(days=(cDays))
 
-    #         elif arDia[1] == 28 and self._Increment > 0:             
-    #             ldom = 30 * self._Increment 
-
-    #         else:
-
-    #             try:
-    #                 validMonth = [1, 3, 5, 7, 8, 10, 12].index(mes)
-    #                 ldom = arDia[1] * self._Increment 
-
-    #             except:
-    #                 if self.varDate.day == 30:
-
-    #                     if calendar.isleap(ano) == True:
-    #                         calcDia = 31
-    #                     else:
-    #                         calcDia = 30
-
-    #                     ldom = calcDia * self._Increment                
-    #                 else:
-    #                     ldom = arDia[1] * self._Increment 
-
-    #         vMonth = self.varDate + timedelta(days=ldom) 
-
-    #         vDia = getAlinhamento(dia,vMonth.month, vMonth.year)   
-    #         vData = date(vMonth.year, vMonth.month ,vDia.day)
-
+            vInterDate = __getAlignment(interDate.year, interDate.month, interDate.day)
+    
     #     # elif vIinterval.upper() == "WEEK":
     #     #     pass
 
-    #     elif self._Interval.upper() == "DAY":
+        elif self._Interval.upper() == "DAY":
             
-    #         vDia =  self.varDate + timedelta(days=self._Increment)
+            interDate =  self.varDate + timedelta(days=self._Increment)
             
-    #         vData = getAlinhamento(vDia.day,vDia.month,vDia.year)        
-    #     self._DataFim = vData
-    #     return vData
+            vInterDate = __getAlignment(interDate.year, interDate.month, interDate.day)    
+
+        # self._DataFim = vData
+
+        return vInterDate
      
     # def getAnoMes(self):
     #     """ getAnoMes: Método que trás o ano mês """
